9244271aea5f879533c46190bb9f5ef37eeeee2f
valmar parts <valmarparts@gmail.com>
2022-10-30 20:12:38 +0200

    Update readme with plans, restructure goals before building projects

--- README.md
+++ README.md
@@ -27,9 +27,26 @@ All Covered topics by timestamps:
 - Events - [1:24:40](https://www.youtube.com/watch?v=hdI2bqOjy3c&t=5080s)
 - Form Script - [1:30:39](https://www.youtube.com/watch?v=hdI2bqOjy3c&t=5439s)
 
+- 8 Must Know JavaScript Array Methods https://www.youtube.com/watch?v=R8rmfD9Y5-c
+
 ---
 
 # Next up
 
+https://www.youtube.com/watch?v=rRgD1yVwIvE - JavaScript Higher Order Functions & Arrays
+
+https://www.youtube.com/watch?v=7W4pQQ20nJg - JavaScript Arrays Crash Course
+
+https://www.youtube.com/watch?v=Oive66jrwBs - Fetch API Introduction
+
+https://www.youtube.com/watch?v=DHvZLI7Db8E - JavaScript Promises In 10 Minutes
+
+https://www.youtube.com/watch?v=PoRJizFvM7s - Async JS Crash Course - Callbacks, Promises, Async Await
+
+---
+
+# Future plans
+
+- React JS Crash Course for school project https://www.youtube.com/watch?v=w7ejDZ8SWv8&t=2s
 - Repository for the project - https://github.com/valpar/Netflix_landing_page_clone
 - Netflix landing page clone with html, css and javascript - https://youtu.be/P7t13SGytRk?t=22
---
+++ arrays.js
@@ -0,0 +1,51 @@
+const items = [
+  { name: "Bike", price: 100 },
+  { name: "Book", price: 15 },
+  { name: "Table", price: 250 },
+  { name: "Phone", price: 780 },
+  { name: "Computer", price: 2400 },
+  { name: "Keyboard", price: 145 },
+  { name: "Apple", price: 1.5 },
+  { name: "T-shirt", price: 25 },
+];
+
+//keeps the old array the same
+// const filteredItems = items.filter((item) => {
+//   return item.price <= 100;
+// });
+
+//takes one array and can convert into a different array
+// const itemNames = items.map((item) => {
+//   return item.name;
+// });
+
+// returns the value of the first element that passes true.
+// const foundItem = items.find((item) => {
+//   return item.name === "Apple";
+// });
+
+// items.forEach((item) => {
+//   console.log(item.name);
+// });
+
+//checks if any of the values is true
+// const cheapItems = items.some((item) => {
+//   return item.price <= 100;
+// });
+
+// const cheapItems = items.every((item) => {
+//   return item.price <= 100;
+// });
+
+// const total = items.reduce((currentTotal, item) => {
+//   return item.price + currentTotal;
+// }, 0);
+
+// console.log(total);
+//console.log(itemNames); //new array
+
+//https://www.youtube.com/watch?v=rRgD1yVwIvE
+//https://www.youtube.com/watch?v=7W4pQQ20nJg
+//https://www.youtube.com/watch?v=Oive66jrwBs
+//https://www.youtube.com/watch?v=DHvZLI7Db8E
+//https://www.youtube.com/watch?v=PoRJizFvM7s
